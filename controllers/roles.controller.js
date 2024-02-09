bb20423438c54521a8784955fc40327aa6f162fc
Yonathan Vasquez <yva4592@gmail.com>
2024-02-09 15:43:42 -0300

    4.1 actividad

--- controllers/roles.controller.js
+++ controllers/roles.controller.js
@@ -1,3 +1,4 @@
+
 const db = require("../models");
 const Roles = db.roles;
 const Users = db.users;
@@ -19,155 +20,127 @@ exports.create = (roles) => {
 };
 
 
-//Crear y guardar nuevos comentarios
 
-exports.createUsers = (rolesId, users) => {
-  return Users.create({
-    name: users.name,
-    last_name: users.last_name,
-    email: users.email,
-    password: users.password,
-    roleId: rolesId,
-  })
-    .then((users) => {
-      console.log(">> Created users: " + JSON.stringify(users, null, 4));
-      return users;
+exports.findAll = (req, res) => {
+  
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+
+  Roles.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
     })
-    .catch((err) => {
-      console.log(">> Error while creating users: ", err);
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving Roless."
+      });
     });
 };
 
 
-  // Obtener todos los tutoriales incluyen comentarios
 
-  exports.findAll = () => {
-    return Roles.findAll({
-      include: ["users"],
-    }).then((roles) => {
-      return roles;
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Roles.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Roles with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Roles with id=" + id
+      });
     });
-  };
+};
 
 
-  exports.findAll = (req, res) => {
-    
-    const title = req.query.title;
-    var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-  
-    Roles.findAll({ where: condition })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving Roless."
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  Roles.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Roles was updated successfully."
         });
-      });
-  };
-
-
-
-  exports.findOne = (req, res) => {
-    const id = req.params.id;
-  
-    Roles.findByPk(id)
-      .then(data => {
-        if (data) {
-          res.send(data);
-        } else {
-          res.status(404).send({
-            message: `Cannot find Roles with id=${id}.`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error retrieving Roles with id=" + id
+      } else {
+        res.send({
+          message: `Cannot update Roles with id=${id}. Maybe Roles was not found or req.body is empty!`
         });
-      });
-  };
-
-
-  exports.update = (req, res) => {
-    const id = req.params.id;
-  
-    Roles.update(req.body, {
-      where: { id: id }
+      }
     })
-      .then(num => {
-        if (num == 1) {
-          res.send({
-            message: "Roles was updated successfully."
-          });
-        } else {
-          res.send({
-            message: `Cannot update Roles with id=${id}. Maybe Roles was not found or req.body is empty!`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error updating Roles with id=" + id
-        });
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Roles with id=" + id
       });
-  };
+    });
+};
 
 
-  exports.delete = (req, res) => {
-    const id = req.params.id;
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Roles.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Roles was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Roles with id=${id}. Maybe Roles was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Roles with id=" + id
+      });
+    });
+};
+
+
+exports.deleteAll = (req, res) => {
+  Roles.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} Roless were deleted successfully!` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all Roless."
+      });
+    });
+};
+
+
+
+
+exports.findAllPublished = (req, res) => {
+  Roles.findAll({ where: { published: true } })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving Roless."
+      });
+    });
+};
+
   
-    Roles.destroy({
-      where: { id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.send({
-            message: "Roles was deleted successfully!"
-          });
-        } else {
-          res.send({
-            message: `Cannot delete Roles with id=${id}. Maybe Roles was not found!`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Could not delete Roles with id=" + id
-        });
-      });
-  };
-
-
-  exports.deleteAll = (req, res) => {
-    Roles.destroy({
-      where: {},
-      truncate: false
-    })
-      .then(nums => {
-        res.send({ message: `${nums} Roless were deleted successfully!` });
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while removing all Roless."
-        });
-      });
-  };
-
-
-
-
-  exports.findAllPublished = (req, res) => {
-    Roles.findAll({ where: { published: true } })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving Roless."
-        });
-      });
-  };
